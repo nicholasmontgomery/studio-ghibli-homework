8f3faa6b437d3a08b384d3f222dcef3e8b12733d
nicholasmontgomery <na_montgomery@yahoo.co.uk>
2020-02-28 14:31:45 +0000

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
-<template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<template lang="html">
+  <h1>Ghilbi Film Studio</h1>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      films: [],
+      selectedFilm: {}
+    };
+  },
+  mounted(){
+    fetch('https://ghibliapi.herokuapp.com/films')
+    .then(result => result.json())
+    .then(films => this.films = films)
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="css" scoped>
 </style>
